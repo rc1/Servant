ef9973595696747e40903798c7979f1b56964eea
Ross Cairns <ross@electricglen.com>
2012-04-12 20:08:03 +0100

    file watching

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-basic express webserver
+# basic express webserver
+
+To setup:
+
+    $ npm install
+
+To start:
+
+    $ node app.js
+
+Specify port:
+
+    $ node app.js -p 8000
+
+#### File watching &amp; auto reloading
+
+To start with file watching:
+
+    $ node app.js -w
+
+to have your webpage auto reloading/refresh, include:
+
+    <script src="/socket.io/socket.io.js"> </script><script src="/reload.js"></script>
+
+
+#### Jade and Express routing
+
+In `app.js` uncomment the following lines:
+
+ 
+    app.get('/', function(req, res) {
+        res.render('index', {
+            title: 'Simple Server',
+            watch: {
+                enabled: watch,
+                script: '<script src="/socket.io/socket.io.js"></script><script src="/reload.js"></script>'
+            }
+        });
+    });
--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // include
 var express = require('express');
 var app = module.exports = express.createServer();
+var lessErrorHandler = require ('connect-less-errors');
 
 // configure
 app.configure(function() {
@@ -13,28 +14,81 @@ app.configure(function() {
     app.use(express.methodOverride());
     app.use(app.router);
     app.use(express.compiler({src: __dirname + '/public/', enable:['less']}));
+    app.use(lessErrorHandler);
     app.use(express.static(__dirname + '/public'));
     app.use(express.directory(__dirname + '/public'));
     app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
-//
 
 // routes
 /*
 app.get('/', function(req, res) {
     res.render('index', {
-        title: 'Root Page'
-     });
- });
+        title: 'Simple Server',
+        watch: {
+            enabled: watch,
+            script: '<script src="/socket.io/socket.io.js"></script><script src="/reload.js"></script>'
+        }
+    });
+});
 */
 
-// start
-if (process.argv.length == 3) {
-    app.listen(process.argv[2]);
-} else if (process.argv.length == 4) {
-    app.listen(process.argv[3]);
-} else {
-    console.log("Listen on default port. Run with `node app.js -p 8080` to specify port");
-    app.listen(9000);
+// watch routes
+app.get("/reload.js", function (req, res) {
+    res.writeHead(200, {'Content-Type': 'application/javascript'});
+    res.end("var socket = io.connect(); socket.on('file_did_change', function (data) {   if (data.file_extension === '.css' || data.file_extension == '.less') { var queryString = '?reload=' + new Date().getTime(); $('link[rel=\"stylesheet\"]').each(function () { if (this.href.indexOf(\"typekit\") === -1) { this.href = this.href.replace(/\\?.*|$/, queryString); } }); } else {   window.location.reload();  }  });");
+});
+
+var argv = require('optimist')
+            .alias('p', 'port')
+            .describe('p', 'port number')
+            .boolean('w')
+            .alias('w', 'watch')
+            .describe('w', 'watch files for changes and reload').argv,
+    port = argv.p || 4000,
+    watch = argv.w || true;
+
+app.listen(port);
+
+if (watch) {
+    var sio = require('socket.io'),
+        watch = require('watch'),
+        path = require('path');
+
+    // use socket.io
+    var io = sio.listen(app);
+    io.set('log level', 0);
+    io.sockets.on('connection', function (socket) { console.log(" -- browser connected."); } );
+
+    // watch files for changes
+    watch.createMonitor(__dirname + '/public', { ignoreDotFiles: true}, function (monitor) {
+        monitor.on("changed", function (f, curr, prev) {
+            console.log(path.extname(f));
+            io.sockets.emit('file_did_change', {
+                file_extension: path.extname(f)
+            }); 
+        });
+        monitor.on("removed", function (f, stat) {
+            io.sockets.emit('file_did_change', {
+                file_extension: path.extname(f)
+            }); 
+        });
+    });
+    watch.createMonitor(__dirname + '/views', { ignoreDotFiles: true}, function (monitor) {
+        monitor.on("changed", function (f, curr, prev) {
+            io.sockets.emit('file_did_change', {
+                file_extension: path.extname(f)
+            }); 
+        });
+        monitor.on("removed", function (f, stat) {
+            io.sockets.emit('file_did_change', {
+                file_extension: path.extname(f)
+            }); 
+        });
+    });
+
+    // tell people about it
+    console.log("Socket.io will annouce file changes");
 }
+
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
