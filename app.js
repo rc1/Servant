dda29146eb52d664801cacbcf54440886ffa6667
Ross Cairns <ross@electricglen.com>
2013-05-21 13:42:15 +0100

    Updated docs and package

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
-
-/**
- * Module dependencies.
- */
-
+// # Module Dependencies.
 var express = require('express');
 var http = require('http');
 var lessMiddleware = require('less-middleware');
 var path = require('path');
+// ## Custom Libs
+var jadeMiddleware = require(path.join(__dirname, 'jade-middleware.js'));
+var liveReload = require(path.join(__dirname, 'live-reload.js'));
 
 var app = express();
-
 app.configure(function(){
     app.set('port', process.env.PORT || 3000);
     app.use(express.bodyParser());
     app.use(express.methodOverride());
+    app.use(jadeMiddleware({
+        src: path.join(__dirname, 'public'),
+        jadeOptions: {
+            pretty: true,
+            filename: path.join(__dirname, 'public/layout.jade')
+        }
+    }));
     app.use(lessMiddleware({
         src: path.join(__dirname, 'public')
     }));
@@ -25,6 +30,19 @@ app.configure('production', function(){
     console.log("Should not be used for production");
 });
 
-http.createServer(app).listen(app.get('port'), function(){
+var server = http.createServer(app);
+server.listen(app.get('port'), function(){
     console.log("Express server listening on port " + app.get('port'));
 });
+
+// # Live Reloading
+// To watch `WATCH=1 node app.js`
+if (process.env.WATCH) {
+    liveReload.watch({
+        pathPatterns: [path.join(__dirname, 'public')+"/**/*"],
+        server: server,
+        app: app
+    });
+    console.log("Watching public and views for changes. Add /watch.js to your html.");
+}
+
---
+++ live-reload.js
@@ -0,0 +1,48 @@
+var WebSocketServer = require('ws').Server;
+var Gaze = require('gaze');
+var path = require('path');
+
+module.exports.watch = function (options) {
+    var server = options.server;
+    var app = options.app;
+    var pathPatterns = options.pathPatterns;
+
+    var wss = new WebSocketServer({server: server});
+    var wsIdCounter = 0;
+    var socketConnections = Object.create(null);
+    wss.on('connection', function(ws) {
+        var cid = "ws"+(++wsIdCounter);
+        socketConnections[cid]=ws;
+        ws.onclose = function () {
+            delete socketConnections[cid];
+        };
+    });
+    var broadcast = function (message) {
+        for (var ws in socketConnections) {
+            socketConnections[ws].send(message);
+        }
+    };
+    app.get("/watch.js", function (req, res) {
+        res.writeHead(200, {'Content-Type': 'application/javascript'});
+        res.end("var socket = new WebSocket('ws://'+window.location.host+'/'); \n\
+            socket.onmessage = function (message) { \n\
+                if (message === '.css' || message == '.less') { \n\
+                    var queryString = '?reload=' + new Date().getTime(); \n\
+                    $('link[rel=\"stylesheet\"]').each(function () { \n\
+                        if (this.href.indexOf(\"typekit\") === -1) { \n\
+                            this.href = this.href.replace(/\\?.*|$/, queryString); \n\
+                        } \n\
+                    }); \n\
+                } else { \n\
+                    window.location.reload(); \n\
+                } \n\
+            };");
+    });
+
+    // Create a watch for each folder
+    var gaze = new Gaze(pathPatterns);
+    gaze.on('all', function(event, filepath) { 
+        broadcast(path.extname(filepath));
+    });
+};
+
