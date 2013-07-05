d521eebb7113538adcbeadc5505f798a301a7838
Ross Cairns <ross@electricglen.com>
2013-07-05 14:14:16 +0100

    Fixed css reloading. Debounced broadcasts.

--- live-reload.js
+++ live-reload.js
@@ -1,6 +1,7 @@
 var WebSocketServer = require('ws').Server;
 var Gaze = require('gaze');
 var path = require('path');
+var _ = require('underscore');
 
 module.exports.watch = function (options) {
     var server = options.server;
@@ -17,16 +18,18 @@ module.exports.watch = function (options) {
             delete socketConnections[cid];
         };
     });
-    var broadcast = function (message) {
+    var broadcast = _.throttle(function (message) {
         for (var ws in socketConnections) {
             socketConnections[ws].send(message);
         }
-    };
+    }, 300);
+
+    var isBroadcastingChange = false;
     app.get("/watch.js", function (req, res) {
         res.writeHead(200, {'Content-Type': 'application/javascript'});
         res.end("var socket = new WebSocket('ws://'+window.location.host+'/'); \n\
             socket.onmessage = function (message) { \n\
-                if (message === '.css' || message == '.less') { \n\
+                if (message.data === '.css' || message.data == '.less') { \n\
                     var queryString = '?reload=' + new Date().getTime(); \n\
                     $('link[rel=\"stylesheet\"]').each(function () { \n\
                         if (this.href.indexOf(\"typekit\") === -1) { \n\
@@ -42,7 +45,7 @@ module.exports.watch = function (options) {
     // Create a watch for each folder
     var gaze = new Gaze(pathPatterns);
     gaze.on('all', function(event, filepath) {
-        console.log("File changed:", filepath); 
+        console.log("File changed:", path.extname(filepath)); 
         broadcast(path.extname(filepath));
     });
 };
